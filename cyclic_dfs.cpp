f7a17c86b07bc09c661f6432494076366a46cb28
Snorlaxx96 <giselobunao@gmail.com>
2025-06-20 11:15:43 +0800

    Update cyclic_dfs.cpp

--- cyclic_dfs.cpp
+++ cyclic_dfs.cpp
@@ -1,75 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <algorithm>
 
 using namespace std;
 
-void printCycle(const vector<int>& parent, int start, int end) {
-    vector<int> cycle;
-    int current = end;
+const int V = 7; 
+const string labels = "ABCDEFG";
 
-    while (current != start) {
-        cycle.push_back(current);
-        current = parent[current];
-    }
-    cycle.push_back(start);
-    cycle.push_back(end);
+bool dfsCycle(int u, vector<vector<int>>& adjMatrix, vector<int>& color, vector<int>& parent, vector<int>& cyclePath) {
+    color[u] = 1; 
 
-    cout << "Cycle found: ";
-    for (auto it = cycle.rbegin(); it != cycle.rend(); ++it) {
-        cout << *it << " ";
-    }
-    cout << endl;
-}
-
-bool dfsCycle(int node, int parentNode, const vector<vector<int>>& adjMatrix,
-              vector<bool>& visited, vector<int>& parentTrack) {
-    visited[node] = true;
-
-    for (int v = 0; v < adjMatrix.size(); ++v) {
-        if (adjMatrix[node][v]) {
-            if (!visited[v]) {
-                parentTrack[v] = node;
-                if (dfsCycle(v, node, adjMatrix, visited, parentTrack)) {
+    for (int v = 0; v < V; ++v) {
+        if (adjMatrix[u][v]) {
+            if (color[v] == 0) { 
+                parent[v] = u;
+                if (dfsCycle(v, adjMatrix, color, parent, cyclePath))
                     return true;
+            } else if (color[v] == 1) { 
+                cyclePath.clear();
+                int temp = u;
+                cyclePath.push_back(v);
+                while (temp != v) {
+                    cyclePath.push_back(temp);
+                    temp = parent[temp];
                 }
-            } else if (v != parentNode) {
-                parentTrack[v] = node;
-                printCycle(parentTrack, v, node);
+                cyclePath.push_back(v);
+                reverse(cyclePath.begin(), cyclePath.end());
                 return true;
             }
         }
     }
+
+    color[u] = 2; 
     return false;
 }
 
-bool isCyclicDFS(const vector<vector<int>>& adjMatrix) {
-    int n = adjMatrix.size();
-    vector<bool> visited(n, false);
-    vector<int> parent(n, -1);
+bool detectCycleDFS(vector<vector<int>>& adjMatrix, vector<int>& cyclePath) {
+    vector<int> color(V, 0); 
+    vector<int> parent(V, -1);
 
-    for (int i = 0; i < n; ++i) {
-        if (!visited[i]) {
-            if (dfsCycle(i, -1, adjMatrix, visited, parent)) {
+    vector<int> order = {3, 0, 1, 2, 4, 5, 6};
+
+    for (int u : order) {
+        if (color[u] == 0) {
+            if (dfsCycle(u, adjMatrix, color, parent, cyclePath))
                 return true;
-            }
         }
     }
+
     return false;
 }
 
 int main() {
     vector<vector<int>> adjMatrix = {
-        {0, 1, 0, 0, 1},
-        {1, 0, 1, 1, 0},
-        {0, 1, 0, 0, 0},
-        {0, 1, 0, 0, 1},
-        {1, 0, 0, 1, 0}
+        {0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 1, 1, 1}, 
+        {1, 0, 0, 0, 1, 0, 0}, 
+        {1, 0, 0, 0, 0, 0, 0}, 
+        {0, 1, 0, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 0, 0}  
     };
 
-    cout << "=== DFS Cycle Detection ===" << endl;
-    if (!isCyclicDFS(adjMatrix)) {
-        cout << "No cycle found using DFS." << endl;
+    vector<int> cyclePath;
+    if (detectCycleDFS(adjMatrix, cyclePath)) {
+        cout << "Cycle detected: ";
+        for (int node : cyclePath)
+            cout << labels[node] << " ";
+        cout << endl;
+    } else {
+        cout << "No cycle detected.\n";
     }
 
     return 0;
